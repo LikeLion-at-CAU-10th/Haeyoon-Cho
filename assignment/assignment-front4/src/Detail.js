c30c4753428bf5cd31e84b2ded582973eb8b8e63
Haeyoon Cho <101980010+Haeyoon-Cho@users.noreply.github.com>
2022-07-26 19:48:58 +0900

    Update Detail.js

--- assignment/assignment-front4/src/Detail.js
+++ assignment/assignment-front4/src/Detail.js
@@ -1,28 +1,21 @@
 import React from "react";
 import styled from "styled-components";
-import data from "./data/Thin.json";
+import data from "./data/Thing.js";
 import { useParams } from "react-router-dom";
 
 const StyledDom = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
-  margin-top: 50vh;
 `;
 
 const Detail = () => {
   const a = useParams();
-  const id = parseInt(a.id); //parseInt를 써서 스트링 타입을 정수타입으로 변환해야 ===를 쓸 수 있음
+  const id = parseInt(a.id); 
   const thing = data.products.filter((product) => product.id === id)[0];
 
   return (
     <>
       <StyledDom>
-        <div>
-          {" "}
-          {thing.id}
-          {thing.title}이지롱!{" "}
-        </div>
       </StyledDom>
     </>
   );
